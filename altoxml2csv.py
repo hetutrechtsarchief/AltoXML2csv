7e158609a98cd46c68bbf9eac87e8d9f9821a139
Rick Companje <rick@companje.nl>
2021-06-21 13:40:49 +0200

    Update altoxml2csv.py

--- altoxml2csv.py
+++ altoxml2csv.py
@@ -4,70 +4,41 @@ import xml.etree.ElementTree as ET
 
 tags = []
 
-def main(): 
-  writer = csv.DictWriter(sys.stdout, fieldnames=["image","imageWidth","imageHeight","id","text","x","y","width","height","confidence"], quoting=csv.QUOTE_NONNUMERIC)
-  writer.writeheader()
-    
-  for filename in sys.argv[1:]:
-    with open(filename) as file:
+writer = csv.DictWriter(sys.stdout, fieldnames=["image","imageWidth","imageHeight","id","text","x","y","width","height","confidence"], quoting=csv.QUOTE_NONNUMERIC)
+writer.writeheader()
 
-      xmlstring = file.read()
-      xmlstring = re.sub(r'\sxmlns="[^"]+"', '', xmlstring, count=1)
-      xml = ET.fromstring(xmlstring) 
-      items = []
+for filename in sys.argv[1:]:
+  with open(filename) as file:
 
-      image = filename.replace("_alto.xml",".jpg").rpartition("/")[-1]
+    xmlstring = file.read()
+    xmlstring = re.sub(r'\sxmlns="[^"]+"', '', xmlstring, count=1)
+    xml = ET.fromstring(xmlstring) 
+    items = []
 
-      imageWidth = xml.find(".//Page").attrib["WIDTH"]
-      imageHeight = xml.find(".//Page").attrib["HEIGHT"]
+    image = filename.replace("_alto.xml",".jpg").rpartition("/")[-1]
 
-      for textline in xml.findall('.//TextLine/String'): # recursive findall with XPath to also allow TextLines within tables
+    imageWidth = xml.find(".//Page").attrib["WIDTH"]
+    imageHeight = xml.find(".//Page").attrib["HEIGHT"]
 
-        item = {}
+    for textline in xml.findall('.//TextLine/String'): # recursive findall with XPath to also allow TextLines within tables
 
-        item["id"] = ""
+      item = {}
 
-        item["image"] = image
-        item["imageWidth"] = int(imageWidth)
-        item["imageHeight"] = int(imageHeight)
+      item["id"] = ""
 
-        text = textline.attrib["CONTENT"]
-       
-        item["text"] = text   #text.text if text!=None else ""
+      item["image"] = image
+      item["imageWidth"] = int(imageWidth)
+      item["imageHeight"] = int(imageHeight)
 
-        item["x"] = int(textline.attrib["HPOS"])
-        item["y"] = int(textline.attrib["VPOS"])
-        item["width"] = int(textline.attrib["WIDTH"])
-        item["height"] = int(textline.attrib["HEIGHT"])
-        item["confidence"] = round(float(textline.attrib["WC"]),2)
+      text = textline.attrib["CONTENT"]
+     
+      item["text"] = text   #text.text if text!=None else ""
 
-        writer.writerow(item)
+      item["x"] = int(textline.attrib["HPOS"])
+      item["y"] = int(textline.attrib["VPOS"])
+      item["width"] = int(textline.attrib["WIDTH"])
+      item["height"] = int(textline.attrib["HEIGHT"])
+      item["confidence"] = round(float(textline.attrib["WC"]),2)
 
-class BoundingBox(object):
-    """
-    A 2D bounding box
-    """
-    def __init__(self, points):
-        if len(points) == 0:
-            raise ValueError("Can't compute bounding box of empty list")
-        self.minx, self.miny = float("inf"), float("inf")
-        self.maxx, self.maxy = float("-inf"), float("-inf")
-        for x, y in points:
-            self.minx = min(x,self.minx)
-            self.maxx = max(x,self.maxx)
-            self.miny = min(y,self.miny)
-            self.maxy = max(y,self.maxy)
+      writer.writerow(item)
 
-    @property
-    def width(self):
-        return self.maxx - self.minx
-    @property
-    def height(self):
-        return self.maxy - self.miny
-    def __repr__(self):
-        return "BoundingBox(minX={}, minY={}, maxX={}, maxY={})".format(
-            self.minx, self.miny, self.maxx, self.maxy)
-
-
-if __name__ == "__main__": 
-  main() 
